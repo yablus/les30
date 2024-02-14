051538636832dd17c6f0052a2bb0d49ce9ada78b
Evgeny Lagunov <eelagunov@gmail.com>
2024-02-14 17:23:43 +0300

    %_

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -8,9 +8,9 @@ import (
 	"strconv"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/yablus/les30/internal/models"
 	"github.com/yablus/les30/internal/requests"
-	"github.com/yablus/les30/internal/run"
 )
 
 type UserStorage interface {
@@ -26,7 +26,32 @@ type UserHandler struct {
 }
 
 func (u *UserHandler) Run() {
-	run.Run()
+	r := setupServer()
+	http.ListenAndServe(":3000", r)
+}
+
+func setupServer() chi.Router {
+	r := chi.NewRouter()
+	//models.NewStorage()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) { // GET "/"
+		w.Write([]byte("OK"))
+	})
+	r.Mount("/users", UserRoutes())
+	http.ListenAndServe(":8080", r)
+	return r
+}
+
+func UserRoutes() chi.Router {
+	r := chi.NewRouter()
+	u := UserHandler{}
+	r.Get("/", u.ListUsers)                // GET /users
+	r.Post("/", u.CreateUser)              // POST /users
+	r.Put("/{id}", u.UpdateUser)           // PUT /users/{id}
+	r.Delete("/", u.DeleteUser)            // DELETE /users
+	r.Post("/make_friends", u.MakeFriends) // POST /users/make_friends
+	r.Get("/{id}/friends", u.GetFriends)   // GET /users/{id}/friends
+	return r
 }
 
 func (u *UserHandler) ListUsers(w http.ResponseWriter, r *http.Request) {
