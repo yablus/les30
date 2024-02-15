0692004bbdbd3e60ea9dd6c149ec7918fd78627e
Evgeny Lagunov <eelagunov@gmail.com>
2024-02-15 11:20:50 +0300

    refactor testStorage

--- main.go
+++ main.go
@@ -79,7 +79,7 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/yablus/les30/internal/handlers"
-	"github.com/yablus/les30/internal/models"
+	"github.com/yablus/les30/testStorage"
 )
 
 func main() {
@@ -99,8 +99,8 @@ func SetupServer() chi.Router {
 
 func UserRoutes() chi.Router {
 	r := chi.NewRouter()
-	//u := &handlers.UserHandler{&test.FakeStorage{}}
-	u := &handlers.UserHandler{models.NewStorage()}
+	u := &handlers.UserHandler{&testStorage.FakeStorage{}}
+	//u := &handlers.UserHandler{models.NewStorage()}
 
 	r.Get("/", u.ListUsers)                // GET /users
 	r.Post("/", u.CreateUser)              // POST /users
--- testStorage/testStorage.go
+++ testStorage/testStorage.go
@@ -1,4 +1,4 @@
-package test
+package testStorage
 
 import "github.com/yablus/les30/internal/models"
 
